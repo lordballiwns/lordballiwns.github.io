5e26bacd65e112760edefb806e74b875588f02a8
Lord Balliwns Kard <interlan@mail.com>
2024-08-15 00:27:35 -0600

    Actualizar script.js
    v1

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-fetch('(https://api.openweathermap.org/data/2.5/forecast?lang=es&lat=16.41&lon=-98.51&appid=c930acc727dc9fd57adb722dd5f93b74&units=metric&mode=xml)')
-.then(response => response.text())
-.then(data => {
-const parser = new DOMParser();
-const xml = parser.parseFromString(data, 'application/xml');
-const pronostico = xml.querySelector('forecast');
-if (!pronostico) {
-console.error('No se encontró el pronóstico');
-return;
-}
-const html = `<h2>Pronóstico del Tiempo</h2> <ul> ${pronostico.querySelectorAll('time').map(time =>`
-<li>
-${time.querySelector('from').textContent} - ${time.querySelector('to').textContent}
-<ul>
-<li>Temperatura: ${time.querySelector('temperature').getAttribute('value')}°C</li>
-<li>Humedad: ${time.querySelector('humidity').getAttribute('value')}%</li>
-<li>Viento: ${time.querySelector('wind').getAttribute('speed')} m/s</li>
-</ul>
-</li>
-`).join('')} </ul>`;
-document.getElementById('pronostico').innerHTML = html;
-})
-.catch(error => console.error('Error:', error));
+function copyAccountNumber() {
+            const accountNumber = '1271 8001 3023 546289';
+            navigator.clipboard.writeText(accountNumber)
+                .then(() => alert('Número de cuenta copiado: ' + accountNumber))
+                .catch(error => console.error('Error al copiar el número de cuenta:', error));
+        }
