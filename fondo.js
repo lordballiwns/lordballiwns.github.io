ab4a88b2c576c77473e62323ed42485b0cf0b596
Lord Balliwns Kard <interlan@mail.com>
2024-08-14 22:34:04 -0600

    Create fondo.js
    v1 Lanzamiento inicial

---
+++ fondo.js
@@ -0,0 +1,17 @@
+function cambiarFondoSegunHora() {
+            const horaActual = new Date().getHours();
+            const body = document.body;
+
+            if (horaActual >= 6 && horaActual < 12) {
+                body.style.background = 'linear-gradient(-10deg, #23A6D5, #23D5AB)';
+            } else if (horaActual >= 12 && horaActual < 18) {
+                body.style.background = 'linear-gradient(-10deg, #23A6D5, #EE7752)';
+            } else if (horaActual >= 18 && horaActual < 20) {
+                body.style.background = 'linear-gradient(-10deg, #23A6D5, #E73C7E)';
+            } else {
+                body.style.background = 'linear-gradient(-10deg, #23A6D5, #1F1C2C)';
+            }
+        }
+
+        // Llama a la función al cargar la página
+        cambiarFondoSegunHora();
