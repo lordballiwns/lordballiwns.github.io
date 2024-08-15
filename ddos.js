6c76417966c8a26ce28ead158ad5250f3e45e83c
Lord Balliwns Kard <interlan@mail.com>
2024-08-15 12:18:41 -0600

    Actualizar ddos.js
    1.0.6 Cambio de atributos

--- ddos.js
+++ ddos.js
@@ -26,18 +26,18 @@ document.getElementById("horaFechados").innerText = `${diaSemana}, ${numeroDia} 
         fetch("https://api.openweathermap.org/data/2.5/forecast?lang=es&lat=16.41&lon=-98.51&appid=c930acc727dc9fd57adb722dd5f93b74&units=metric")
 .then(response => response.json())
 .then(data => {
-    const clima2 = data.list[8];
-                document.getElementById("clima2").innerText = `
-    Temperatura: ${clima2.main.temp}°C
-    Sensación térmica: ${clima2.main.feels_like}°C
-    Humedad: ${clima2.main.humidity}%
-    Nubosidad: ${clima2.clouds.all}%
-    Descripción del clima: ${clima2.weather[0].description}
-    Probabilidad de lluvia: ${clima2.pop * 100}%
-    Visibilidad: ${clima2.visibility / 1000} km
+    const climados = data.list[8];
+                document.getElementById("climados").innerText = `
+    Temperatura: ${climados.main.temp}°C
+    Sensación térmica: ${climados.main.feels_like}°C
+    Humedad: ${climados.main.humidity}%
+    Nubosidad: ${climados.clouds.all}%
+    Descripción del clima: ${climados.weather[0].description}
+    Probabilidad de lluvia: ${climados.pop * 100}%
+    Visibilidad: ${climados.visibility / 1000} km
                 `;
             })
 .catch(error => {
     console.error("Error al obtener datos del clima:", error);
-                document.getElementById("clima2").innerText = "Error al cargar datos del clima.";
+                document.getElementById("climados").innerText = "Error al cargar datos del clima.";
             });
