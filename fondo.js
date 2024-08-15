631bd9ad11ca50ab9ea961532aa306069af8d678
Lord Balliwns Kard <interlan@mail.com>
2024-08-14 22:45:00 -0600

    Actualizar fondo.js
    v1.1 Corrección de código fuente

--- fondo.js
+++ fondo.js
@@ -1,17 +1,18 @@
+// fondo.js
 function cambiarFondoSegunHora() {
-            const horaActual = new Date().getHours();
-            const body = document.body;
+    const horaActual = new Date().getHours();
+    const body = document.body;
 
-            if (horaActual >= 6 && horaActual < 12) {
-                body.style.background = 'linear-gradient(-10deg, #23A6D5, #23D5AB)';
-            } else if (horaActual >= 12 && horaActual < 18) {
-                body.style.background = 'linear-gradient(-10deg, #23A6D5, #EE7752)';
-            } else if (horaActual >= 18 && horaActual < 20) {
-                body.style.background = 'linear-gradient(-10deg, #23A6D5, #E73C7E)';
-            } else {
-                body.style.background = 'linear-gradient(-10deg, #23A6D5, #1F1C2C)';
-            }
-        }
+    if (horaActual >= 6 && horaActual < 12) {
+        body.style.background = 'linear-gradient(-10deg, #23A6D5, #23D5AB)';
+    } else if (horaActual >= 12 && horaActual < 18) {
+        body.style.background = 'linear-gradient(-10deg, #23A6D5, #EE7752)';
+    } else if (horaActual >= 18 && horaActual < 20) {
+        body.style.background = 'linear-gradient(-10deg, #23A6D5, #E73C7E)';
+    } else {
+        body.style.background = 'linear-gradient(-10deg, #23A6D5, #1F1C2C)';
+    }
+}
 
-        // Llama a la función al cargar la página
-        cambiarFondoSegunHora();
+// Llama a la función al cargar la página
+cambiarFondoSegunHora();
