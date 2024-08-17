43f2b6f82963b6ca9946db21a9bbf2ddcfcb5cca
Lord Balliwns Kard <interlan@mail.com>
2024-08-17 11:22:31 -0600

    Create bordem.js
    1.0.0 Lanzamiento inicial

---
+++ bordem.js
@@ -0,0 +1,17 @@
+var currentTime = new Date().getHours();
+var gradientColors = [
+    'linear-gradient(to right, #ff6347, #ffd700)', // Amanecer
+    'linear-gradient(to right, #ffd700, #87ceeb)', // Día
+    'linear-gradient(to right, #87ceeb, #00008b)'  // Anochecer
+];
+
+var gradientIndex = 0;
+if (currentTime >= 6 && currentTime < 12) {
+    gradientIndex = 0; // Amanecer
+} else if (currentTime >= 12 && currentTime < 18) {
+    gradientIndex = 1; // Día
+} else {
+    gradientIndex = 2; // Anochecer
+}
+
+document.querySelector('.my-button').style.borderImage = gradientColors[gradientIndex];
