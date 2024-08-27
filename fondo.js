871f2d9d8f4620b164f7f8ad8113b4c48edfb574
Lord Balliwns <p.tiggerswan@gmail.com>
2024-08-27 12:00:35 -0600

    Actualizar fondo.js
    Novedades. Actualizamos el tema actual. Ahora los colores son de acuerdo a la hora del día.

--- fondo.js
+++ fondo.js
@@ -1,11 +1,12 @@
+// v3.2.7
 function cambiarFondoSegunHora() {
             const horaActual = new Date().getHours();
             const body = document.body;
 
-            if (horaActual >= 6 && horaActual < 12) {
-                body.style.background = 'linear-gradient(-10deg, #23A6D5, #23D5AB)';
-            } else if (horaActual >= 12 && horaActual < 18) {
+            if (horaActual >= 5 && horaActual < 7) {
                 body.style.background = 'linear-gradient(-10deg, #23A6D5, #EE7752)';
+            } else if (horaActual >= 7 && horaActual < 18) {
+                body.style.background = 'linear-gradient(-10deg, #23A6D5, #23D5AB)';
             } else if (horaActual >= 18 && horaActual < 20) {
                 body.style.background = 'linear-gradient(-10deg, #23A6D5, #E73C7E)';
             } else {
