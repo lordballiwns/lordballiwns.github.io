bf47549c5e479bd199d4e018cc5f0abeea0dbff0
Lord Balliwns <p.tiggerswan@gmail.com>
2024-09-14 22:43:52 -0600

    Actualizar script.js
    2.99

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // informacion.js
 const version = "6";
-const compilacion = "2.95";
+const compilacion = "2.99";
 
 document.addEventListener("DOMContentLoaded", () => {
     const versionElement = document.getElementById("version");
