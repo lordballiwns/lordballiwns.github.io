7259c78df6eb5597fd0bb94b0087eb0299ae0ee0
Lord Balliwns <interlan@mail.com>
2024-10-30 10:45:56 -0600

    Delete captura.js
    Obsolencia

--- captura.js
+++
@@ -1,8 +0,0 @@
-function capture() {
-    html2canvas(document.body).then(canvas => {
-      let a = document.createElement("a");
-      a.download = "clima.png";
-      a.href = canvas.toDataURL("image/png");
-      a.click(); // Puede que no siempre funcione debido a restricciones de seguridad.
-    });
-  }
