06eac2e9554095a0fefa768ab0a603935502e47b
Lord Balliwns <p.tiggerswan@gmail.com>
2024-12-30 23:50:46 -0600

    Actualizar anico.js
    Nightly. Se cambiaron algunos valores. Ahora se muestran horas enteros en el pronóstico secundario. Consultar cambios en el historial.

--- anico.js
+++ anico.js
@@ -7,7 +7,51 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lang=es&lat=17.55&lon=-9
         const clima18 = data.list[6];
         const fecha = new Date(clima0.dt * 1000);
         const opcionesFecha = { weekday: 'long', day: 'numeric', month: 'long', hour: 'numeric', minute: 'numeric', hour12: true };
+fetch('https://api.openweathermap.org/data/2.5/forecast?lang=es&lat=17.55&lon=-99.50&appid=c930acc727dc9fd57adb722dd5f93b74&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        const climaDiv = document.getElementById('clima');
+        const clima0 = data.list[0];
+        const clima9 = data.list[2];
+        const clima18 = data.list[4];
+        const fecha = new Date(clima0.dt * 1000);
+        const opcionesFecha = { weekday: 'long', day: 'numeric', month: 'long', hour: 'numeric', minute: 'numeric', hour12: true };
 
+        // Obtener la hora actual y sumarle 6 horas redondeando los minutos a 00
+        const horaConsulta = new Date();
+        horaConsulta.setHours(horaConsulta.getHours() + 6);
+        horaConsulta.setMinutes(0, 0, 0);
+        const opcionesHora = { hour: 'numeric', minute: 'numeric', hour12: true };
+
+        climaDiv.innerHTML = `
+            <h2>${fecha.toLocaleDateString('es-ES', opcionesFecha)}:</h2>
+            <ul>
+                <li>Temperatura: ${clima0.main.temp}°C</li>
+                <li>Sensación térmica: ${clima0.main.feels_like}°C</li>
+                <li>Humedad: ${clima0.main.humidity}%</li>
+                <li>Nubes: ${clima0.clouds.all}%</li>
+                <li>Descripción: ${clima0.weather[0].description}</li>
+                <li>Probabilidad de lluvia: ${clima0.pop * 100}%</li>
+                <li>Precipitación anterior (3 horas): ${clima0.rain ? clima0.rain["3h"] : 0} mm</li>
+                <li>Población: ${data.city.population}</li>
+                <li>Visibilidad: ${clima0.visibility / 1000} km</li>
+                <li>Velocidad del viento: ${clima0.wind.speed} m/s</li>
+                <li>Presión atmosférica: ${clima0.main.pressure} hPa</li>
+            </ul>
+            <hr>
+            <ul>
+                <li>Temperatura: ${clima9.main.temp}°C</li>
+                <li>Sensación térmica: ${clima9.main.feels_like}°C</li>
+                <li>Humedad: ${clima9.main.humidity}%</li>
+                <li>Nubes: ${clima9.clouds.all}%</li>
+                <li>Descripción: ${clima9.weather[0].description}</li>
+                <li>Probabilidad de lluvia: ${clima9.pop * 100}%</li>
+                <li>Velocidad del viento: ${clima9.wind.speed} m/s</li>
+                <li>Presión atmosférica: ${clima9.main.pressure} hPa</li>
+            </ul>
+            <p>Hora de consulta: ${horaConsulta.toLocaleTimeString('es-ES', opcionesHora)}</p>
+        `;
+    });
         // Obtener la hora actual y sumarle 9 horas
         const horaConsulta = new Date();
         horaConsulta.setHours(horaConsulta.getHours() + 9);
@@ -39,6 +83,6 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lang=es&lat=17.55&lon=-9
                 <li>Velocidad del viento: ${clima9.wind.speed} m/s</li>
                 <li>Presión atmosférica: ${clima9.main.pressure} hPa</li>
             </ul>
-            <p>Hora de consulta: ${horaConsulta.toLocaleTimeString('es-ES', opcionesHora)}</p>
+            <p>pronóstico a 6 horas: ${horaConsulta.toLocaleTimeString('es-ES', opcionesHora)}</p>
         `;
     });
