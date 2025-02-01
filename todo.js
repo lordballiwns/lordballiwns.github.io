6cb72adf29dba97050d1bd4dbdd0d92c969974d9
Lord Balliwns <p.tiggerswan@gmail.com>
2025-02-01 10:00:00 -0600

    Actualizar todo.js
    Transición a Zephyr Rising en su versión 8.0

--- todo.js
+++ todo.js
@@ -5,34 +5,34 @@ function cambiarFondoSegunHora() {
 
     if (horaActual >= 5 && horaActual < 6) {
         // Amanecer inicial
-        body.style.background = 'linear-gradient(-10deg, #FFDDC1, #F8B195)';
+        body.style.background = 'linear-gradient(-10deg, #1E0000, #00004B)';
     } else if (horaActual >= 6 && horaActual < 7) {
         // Amanecer mayor
-        body.style.background = 'linear-gradient(-10deg, #FFD1B3, #F67280)';
+        body.style.background = 'linear-gradient(-10deg, #691900, #00004B)';
     } else if (horaActual >= 7 && horaActual < 10) {
         // Mañana
-        body.style.background = 'linear-gradient(-10deg, #FFB4A2, #6C5B7B)';
+        body.style.background = 'linear-gradient(-10deg, #C8826E, #0B78DC)';
     } else if (horaActual >= 10 && horaActual < 12) {
         // Mediodía
-        body.style.background = 'linear-gradient(-10deg, #FFD166, #f6e8c3)';
+        body.style.background = 'linear-gradient(-10deg, #0B78DC, #0B78DC)';
     } else if (horaActual >= 12 && horaActual < 15) {
         // Tarde inicial
-        body.style.background = 'linear-gradient(-10deg, #FDCB77, #EF476F)';
+        body.style.background = 'linear-gradient(-10deg, #325096, #005AAA)';
     } else if (horaActual >= 15 && horaActual < 17) {
         // Tarde media
-        body.style.background = 'linear-gradient(-10deg, #4ecdc4, #556270)';
+        body.style.background = 'linear-gradient(-10deg, #0050B4, #0046BE)';
     } else if (horaActual >= 17 && horaActual < 18) {
         // Atardecer inicial
-        body.style.background = 'linear-gradient(-10deg, #282c34, #4b4e53)';
+        body.style.background = 'linear-gradient(-10deg, #3C4173, #464B7D)';
     } else if (horaActual >= 18 && horaActual < 20) {
         // Atardecer mayor
-        body.style.background = 'linear-gradient(-10deg, #2e3642, #55a2c2)';
+        body.style.background = 'linear-gradient(-10deg, #282D5F, #323769)';
     } else if (horaActual >= 20 && horaActual < 22) {
         // Noche temprana
-        body.style.background = 'linear-gradient(-10deg, #1b2e3f, #0e4558)';
+        body.style.background = 'linear-gradient(-10deg, #14194B, #1E235F)';
     } else if (horaActual >= 22 || horaActual < 5) {
         // Noche profunda
-        body.style.background = 'linear-gradient(-10deg, #0d1543, #000022)';
+        body.style.background = 'linear-gradient(-10deg, #0A0F41, #000023)';
     }
 }
 
